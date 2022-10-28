8c80b12980ffbfed0ef962bb08494fa744dad114
arunbhalli <tomarbhalli@gmail.com>
2022-10-28 23:46:34 +0200

    adds welcome and date page response

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
+def welcome(request):
+  return HttpResponse('Welome to Meeting planner')
 # Create your views here.
+
+def date(request):
+  return HttpResponse('This page was served at '+ str(datetime.now()))
