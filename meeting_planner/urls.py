016b3650782f522165fbe54cb0af5e8fc95b23ef
arunbhalli <tomarbhalli@gmail.com>
2022-10-29 13:34:25 +0200

    did migration and add meetings app

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import welcome
-from website.views import date
+from website.views import date,about
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome.html',welcome),
-    path('date',date)
+    path('',welcome),
+    path('date',date),
+    path('about',about),
 ]
--- website/views.py
+++ website/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from datetime import datetime
 
+
 def welcome(request):
-  return HttpResponse('Welome to Meeting planner')
+    return HttpResponse('Welome to Meeting planner')
 # Create your views here.
 
+
 def date(request):
-  return HttpResponse('This page was served at '+ str(datetime.now()))
+    return HttpResponse('This page was served at ' + str(datetime.now()))
+
+def about(request):
+  return HttpResponse('I am Arun and i am a good planner ')
