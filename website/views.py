2573797721442bae64d203d0ce6688e9bee6cceb
arunbhalli <tomarbhalli@gmail.com>
2022-10-31 20:57:57 +0100

    adds error message if page is not available

--- website/views.py
+++ website/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 from datetime import datetime
 from meetings.models import Meeting
 
+
 def welcome(request):
-    return render(request,'website/welcome.html',{"num_meetings": Meeting.objects.count()})
+    return render(request, 'website/welcome.html', {"num_meetings": Meeting.objects.count()})
 # Create your views here.
 
 
 def date(request):
     return HttpResponse('This page was served at ' + str(datetime.now()))
 
+
 def about(request):
-  return HttpResponse('I am Arun and i am a good planner ')
+    return HttpResponse('I am Arun and i am a good planner ')
